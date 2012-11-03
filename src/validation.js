19eb5e2f85e855684ad2a60ab24cc16fec00f07a
Ashit Vora <a.k.vora@gmail.com>
2012-11-03 17:46:51 +0530

    Code Refactoring
    - Now able to use parameters in Error message.
    - Fixed validation regex for "Required".
    - Fixed validation regex of ZipCode.
    - Fixed validation regex of CreditCard numbers. Now works for Visa,
    Master Card, Discover, American Express
    - Added validations for
    * alpha-dash
    * size
    * between
    * min
    * max
    * url
    * ipaddress
    * image
    - Now validations can be parameterised

--- src/validation.js
+++ src/validation.js
@@ -15,9 +15,16 @@
 	/**
 	 * Generate error message
 	 */
-	var generate_error_message = function(field, message){
-		return message.replace( "{name}", $(field).attr("name") )
-					  .replace( "{val}", $(field).val() );
+	var generate_error_message = function(field, message, params){
+
+		message = message.replace( "{name}", $(field).attr("name") )
+					  	 .replace( "{val}", $(field).val() );
+
+		$.each(params, function(i, param){
+			message = message.replace("{param"+ (i+1) +"}", param);
+		});
+
+		return message;
 	};
 	
 	
@@ -29,7 +36,7 @@
 	var validations = {
 		required: {
 			rule: function(f){
-				return $(f).val().match(/[\s]+/);
+				return $(f).val().match(/[\S]+/);
 			},
 			message: "{name} can not be blank"
 		},
@@ -65,7 +72,7 @@
 		},
 		uszip: {
 			rule: function(f){
-				return $(f).val().match(/^(\d{5}\-?(\d{4})? )$/);
+				return $(f).val().match(/^(\d{5}\-?(\d{4})?)$/);
 			},
 			message: "{val} is not a valid zipcode"
 		},
@@ -77,16 +84,86 @@
 		},
 		creditcard: {
 			rule: function(f){
-				return $(f).val().match(/^((4\d{3})|(5[1-5]\d{2})|(6011))([- ])?\d{4}([- ])?\d{4}([- ])?\d{4}|3[4,7]\d{13}$/);
+				return $(f).val().match(/^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/);
 			},
-			message: "{val} is not a valid social security number"
+			message: "{val} is not a valid credit card number"
 		},
 		ssn: {
 			rule: function(f){
 				return $(f).val().match(/(^|\s)(00[1-9]|0[1-9]0|0[1-9][1-9]|[1-6]\d{2}|7[0-6]\d|77[0-2])(-?|[\. ])([1-9]0|0[1-9]|[1-9][1-9])\3(\d{3}[1-9]|[1-9]\d{3}|\d[1-9]\d{2}|\d{2}[1-9]\d)($|\s|[;:,!\.\?])/);
 			},
 			message: "{val} is not a valid social security number"
+		},
+		alpha_dash: {
+			rule: function(f){
+				return $(f).val().match(/^([-a-z0-9_-])+$/);
+			},
+			message: "{name} must contain only letters, numbers, dashes or underscore characters."
+		},
+		size: {
+			rule: function(f, params){
+				return $(f).val().length == params[0];
+			},
+			message: "{name} must be exactly {param1} characters long."
+		},
+		between: {
+			rule: function(f, params){
+				value = $(f).val();
+
+				if( isNaN(value) ){
+					return value.length > params[0] && value.length < params[1];
+				}
+				else{
+					return value > params[0] && value < params[1];
+				}
+			},
+			message: "{name} must be between {param1} and {param2}."
+		},
+		min: {
+			rule: function(f, params){
+				value = $(f).val();
+
+				if( isNaN(value) ){
+					return value.length >= parseInt(params[0]);
+				}
+				else{
+					return parseInt(value) >= parseInt(params[0]);
+				}
+			},
+			message: "{name} must be minimum {param1}."
+		},
+		max: {
+			rule: function(f, params){
+				value = $(f).val();
+
+				if( isNaN(value) ){
+					return value.length <= parseInt(params[0]);
+				}
+				else{
+					return parseInt(value) <= parseInt(params[0]);
+				}
+			},
+			message: "{name} should not be more than {param1}."
+		},
+		url: {
+			rule: function(f){
+				return $(f).val().match(/^\b(https?|ftp|file):\/\/[-A-Za-z0-9+&@#\/%?=~_|!:,.;]*[-A-Za-z0-9+&@#\/%=~_|]/);
+			},
+			message: "{val} is not a valid URL"
+		},
+		ipaddress: {
+			rule: function(f){
+				return $(f).val().match(/^(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))$/);
+			},
+			message: "{val} is not a valid IP address"
+		},
+		image: {
+			rule: function(f){
+				return $(f).val().match(/(.*?)\.(jpg|jpeg|png|gif|bmp)$/i);
+			},
+			message: "{val} is not an image."
 		}
+
 	};
 	
 	
@@ -106,8 +183,6 @@
 				message: message
 			};
 		}
-		
-		console.log(validations);
 	};
 
 
@@ -125,13 +200,21 @@
 			var validations_to_test = $(field).attr("data-validate").split("|");
 			
 			$.each(validations_to_test, function(j, v){
-				v = $.trim(v).toLowerCase();
-				
-				console.log("Testing for: "+ v);
+				// required, email, size[5], between[1,10]
+				v_params = $.trim(v).toLowerCase().split("[");
+
+				v = v_params[0];
+
+				if(v_params.length > 1){
+					params = v_params[1].replace("]", "").split(",");
+				}
+				else{
+					params = [];
+				}
 				
 				if(validations.hasOwnProperty (v) ){
-					if(! validations[v].rule(field) ){
-						errors.push( generate_error_message( field, validations[v].message) );
+					if(! validations[v].rule(field, params) ){
+						errors.push( generate_error_message( field, validations[v].message, params) );
 					}
 				}
 			});			
